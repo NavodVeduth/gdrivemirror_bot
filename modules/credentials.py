65f236e41f62fbd193bdb7a39a354e743463c201
linkonly <53618953+linkonly@users.noreply.github.com>
2019-08-30 15:16:56 +0530

    Add files via upload

---
+++ modules/credentials.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 Atul-Kadian
+
+class Creds(object):
+        TOKEN = "734568520:AAFd1ck3I5NePnEvMaAvx4sDyNVNckwLULA"
+        CLIENT_ID = "935308063778-37ro1u06d01k110j98eqh2m6flq64kf3.apps.googleusercontent.com"
+        CLIENT_SECRET = "t7_O-5JeWokzF1m2Ldk-2z7Y"
+
