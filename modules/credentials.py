b8eadbd5315cca8988be8c6119e9aa54515ed99d
Navod Veduth <54998118+NavodVeduth@users.noreply.github.com>
2019-09-07 16:24:46 +0530

    Update credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2019 Atul-Kadian
 
 class Creds(object):
-        TOKEN = "846580966:AAFaRoniTzokiPjchHbRSaz2yP8vCxoL7ZU"
-        CLIENT_ID = "935308063778-37ro1u06d01k110j98eqh2m6flq64kf3.apps.googleusercontent.com"
-        CLIENT_SECRET = "t7_O-5JeWokzF1m2Ldk-2z7Y"
+        TOKEN = ""
+        CLIENT_ID = "ps.googleusercontent.com"
+        CLIENT_SECRET = "-2z7Y"
 
